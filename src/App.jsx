836bd34ea2835cb5dd2370bb50816af76cf88e9e
Shiqwan <ulrika.galnander@gmail.com>
2023-11-02 20:48:53 +0100

    Baby steps

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import { Top } from "./Header/Top";
+
+export const App = () => {
+  return (
+    <div className="topper">
+      <Top />
+    </div>
+  );
+};
---
+++ src/Header/Top.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../Header/Top.css";
+
+export const Top = () => {
+  return (
+    <div className="topper">
+      <h1>HEllooo</h1>
+    </div>
+  );
+};
