d4222404e9f56d9df4226dd24b299581a28eb520
Shiqwan <ulrika.galnander@gmail.com>
2023-11-11 13:54:53 +0100

    General commit

--- src/Components/C Featured Projects/CardProject.jsx
+++ src/Components/C Featured Projects/CardProject.jsx
@@ -6,16 +6,20 @@ export const CardProject = (props) => {
   let imgProject = props.projects.image;
   let nameProject = props.projects.name;
   let descriptionProject = props.projects.description;
+  let tagsProject = props.projects.tags;
 
   return (
     <>
       <div className="img-wrapper">
-        <img src={imgProject} alt="nameProject" className="project-img" />
+        <img src={imgProject} alt={nameProject} className="project-img" />
       </div>
       <div className="card-wrapper">
         <div className="info-wrapper">
           <CardProjectHeading className="project-head" text={nameProject} />
-          <Description className="project-paragraph" />
+          <Description
+            className="project-paragraph"
+            text={descriptionProject}
+          />
         </div>
       </div>
     </>
